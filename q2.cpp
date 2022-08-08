a683a8fc1ac86b09e2d1e268858a83b08691a4c9
Guneet Kaur Bhatia <91559944+guneet-01@users.noreply.github.com>
2022-08-08 14:05:26 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -6,9 +6,7 @@ int main()
 	int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
 	int n = sizeof(arr) / sizeof(arr[0]);
 
-	/*Here we take two parameters, the beginning of the
-	array and the length n upto which we want the array to
-	be sorted*/
+	
 	sort(arr, arr + n);
 
 	cout << "\nArray after sorting using "
